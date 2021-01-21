622ff57b6ce21ce518959fc68cb233c64e943621
Victor-maestro <victor_maestro@mail.ru>
2021-01-21 13:50:07 +0300

    add access & edit main structure

--- app/js/main.js
+++ app/js/main.js
@@ -1,24 +1,24 @@
 /* jQuery */
 $(function() {
 	
-	// Mask form
-	$('input[name="Lead[phone]"]').mask('8 (999) 999 - 99 - 99');
+	$('.radio').on('click', function (e) {
+
+		let radioInput = $(this).children('.radio__input');
+
+		if (e.target !== radioInput[0]) {
+			radioInput.click();
+		}
+
+		$('.radio').children('.show-field').removeClass('show');
+
+		$(this).children('.show-field').addClass('show');
+		
+	});
+
 
 
 });
 
-/* Javascript */
 
-// Этот медиа-запрос нацелен на области просмотра, которые имеют минимальную ширину 320 пикселей
-const mQuery = window.matchMedia('(min-width: 540px)')
 
-function handleMobilePhoneResize(e) {   
-   // Проверяем, верен ли медиа-запрос
-	if (e.matches) {     
-		// Затем выводим в консоль следующее сообщение
-		console.log('Media Query Matched!')   
-	} 
-} 
 
-// Настраиваем слушателя событий
-mQuery.addListener(handleMobilePhoneResize)
